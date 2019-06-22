c78ff5618976f6912c1a1eac839d6b05963a847a
tuquynh <tuquynh141088@gmail.com>
2019-06-22 17:44:27 +0200

    Change default page to 404.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x8e-nq2+*bqftnvib9qk*_x1j(_dh&&t-f$4(q&b(%#-k^ui)+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
